3216087858e6adf71e0c6f666c675662f705c535
Stanislav Taran <StanislavTaran@users.noreply.github.com>
2020-03-26 19:47:03 +0200

    added first task

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from './Profile.module.css';
+import propTypes from 'prop-types';
+
+const Profile = ({ avatar, name, tag, location, stats }) => {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="user avatar" className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li className={styles.statsItem}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li className={styles.statsItem}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
+        </li>
+        <li className={styles.statsItem}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.defaultProps = {
+  avatar:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5s9n8MG2RJPcqJSEXvRjGCckFRyCbyatb_DW7NgMy07cD5arh',
+  location: 'Unknown',
+};
+
+Profile.propTypes = {
+  avatar: propTypes.string,
+  name: propTypes.string.isRequired,
+  tag: propTypes.string.isRequired,
+  location: propTypes.string,
+  stats: propTypes.shape({
+    followers: propTypes.number.isRequired,
+    views: propTypes.number.isRequired,
+    likes: propTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Profile;
