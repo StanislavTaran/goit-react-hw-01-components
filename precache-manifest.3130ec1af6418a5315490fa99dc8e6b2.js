c5d725669f7e80ba6029b3f650862cba733a142a
Stanislav Taran <StanislavTaran@users.noreply.github.com>
2020-03-28 16:49:03 +0200

    Updates

---
+++ precache-manifest.3130ec1af6418a5315490fa99dc8e6b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5ed5b731c15aa0f62305014fd5085e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "01b6b8d26cf4f3c33e49",
+    "url": "/goit-react-hw-01-components/static/css/main.ca9a636c.chunk.css"
+  },
+  {
+    "revision": "8c72e9ff135232328258",
+    "url": "/goit-react-hw-01-components/static/js/2.de823e09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.de823e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01b6b8d26cf4f3c33e49",
+    "url": "/goit-react-hw-01-components/static/js/main.a2c5bd67.chunk.js"
+  },
+  {
+    "revision": "2c90fadf0dd2bf61ff2f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.1ea492af.js"
+  }
+]);
